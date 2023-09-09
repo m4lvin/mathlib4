3926293fb82de6c4b0b2cb7d0a45386262b7e83b
Linus <95619282+linus-md@users.noreply.github.com>
2023-09-09 20:28:26 +0200

    init

---
+++ Mathlib/Algebra/Tropical/Polynomials.lean
@@ -0,0 +1,23 @@
+/-
+Copyright (c) 2023 Linus A. Sommer. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Linus A. Sommer.
+-/
+
+-- Goals:
+  -- Define The a polynomial ring over the
+  -- Prove: tropical polynomials are the minimum over a finite piecewise collection of linear functions.
+  -- Prove: Tropical Fundamental Theorem of Algebra
+
+import Mathlib.Algebra.Tropical.Basic
+import Mathlib.Data.Polynomial.Basic
+import Mathlib.Data.Real.ENNReal
+
+open Tropical ENNReal
+
+open scoped Polynomial
+
+universe u v
+variable {f : (Tropical ℝ≥0∞)[X]}
+
+#check f 
---
+++ Mathlib/AlgebraicGeometry/TropicalGeometry/Hypersurface.lean
@@ -0,0 +1,13 @@
+/-
+Copyright (c) 2023 Linus A. Sommer. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Linus A. Sommer.
+-/
+
+import Mathlib.Algebra.Tropical.Basic
+import Mathlib.Data.Polynomial.Basic
+import Mathlib.Data.Real.ENNReal
+
+open Tropical ENNReal
+open scoped Polynomial
+
